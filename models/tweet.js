625d385b3352b9f4b1226adc216baf87d83f9d92
Sanjeev Singh <sanjeevsinghsaini48@gmail.com>
2024-03-26 01:17:59 +0530

    added hashtag model and tweet model

--- models/tweet.js
+++ models/tweet.js
@@ -5,29 +5,19 @@ const tweetSchema=new mongoose.Schema({
     content:{
         type:String,
         requried:true,
+        max:[250,"Tweet cannot be more than 250 characters"]
     },
-    userEmail:{
-        type:String,
+    hashtags:[
+        {
+            type:mongoose.Schema.Types.ObjectId,
+            ref:'Hashtag'
+        }
+    ]
 
-    },
-    comments:{
-        type:mongoose.Schema.Types.ObjectId,  //reference to
-        ref:"Comment",
-    }
 },{
     timestamps:true
 })
 
-
-tweetSchema.virtual('contentWithEmail').get(function process(){
-    return `${this.content} \nCreated by:${this.userEmail}`
-})
-
-tweetSchema.pre('save',function(next){
-    console.log("inside a hook");
-    this.content=this.content + '.....',
-    next();
-})
 
 const Tweet=mongoose.model('Tweet',tweetSchema);
 
